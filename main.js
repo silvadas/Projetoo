c72250ac9f3096d85edd0056673f6a45bd6d6bda
silvadas <felix.barbosa.gabriel@escola.pr.gov.br>
2026-06-17 08:39:24 -0300

    ..

--- main.js
+++ main.js
@@ -3,42 +3,42 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 
 for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-      textos[j].classList.remove("ativo");
-    }
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
 
-    botoes[i].classList.add("ativo");
-    textos[i].classList.add("ativo");
-  };
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    };
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-11-30");
+const tempoObjetivo1 = new Date("2026-12-18");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-  let segundos = Math.floor(tempoFinal / 1000);
-  let minutos = Math.floor(segundos / 60);
-  let horas = Math.floor(minutos / 60);
-  let dias = Math.floor(horas / 24);
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-  segundos %= 60;
-  minutos %= 60;
-  horas %= 24;
-  
-  return (
-  dias +
-  " dias " +
-  horas +
-  " horas " +
-  minutos +
-  " minutos " +
-  segundos +
-  " segundos"
-);
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return (
+        dias +
+        " dias " +
+        horas +
+        " horas " +
+        minutos +
+        " minutos " +
+        segundos +
+        " segundos"
+    );
 }
