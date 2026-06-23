5a471a1819eb6e0ff40af88e323579fb618477ce
silvadas <felix.barbosa.gabriel@escola.pr.gov.br>
2026-06-23 09:33:52 -0300

    ...

--- main.js
+++ main.js
@@ -34,14 +34,17 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 
-    return (
-        dias +
-        " dias " +
-        horas +
-        " horas " +
-        minutos +
-        " minutos " +
-        segundos +
-        " segundos"
-    );
-}
+    if (tempoFinal > 0) {
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+} else {
+  return "Prazo Finalizado";
+}.
