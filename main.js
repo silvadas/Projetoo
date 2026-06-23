74137f16e328687277ea0b75b9dd14e4344dc700
silvadas <felix.barbosa.gabriel@escola.pr.gov.br>
2026-06-23 09:30:19 -0300

    erg

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-12-18");
+const tempoObjetivo2 = new Date("2027-03-02");
+const tempoObjetivo3 = new Date("2026-11-08");
+const tempoObjetivo4 = new Date("2026-10-09");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
