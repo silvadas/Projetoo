34ad5405cae6f7f2800ad176ab48dd75d9d47dbf
silvadas <felix.barbosa.gabriel@escola.pr.gov.br>
2026-06-24 08:30:35 -0300

    ..

--- main.js
+++ main.js
@@ -3,53 +3,42 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 
 for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
 
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    };
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-12-18");
-const tempoObjetivo2 = new Date("2027-03-02");
-const tempoObjetivo3 = new Date("2026-11-08");
-const tempoObjetivo4 = new Date("2026-10-09");
+const tempoObjetivo1 = new Date("2026-11-30");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-contadores[1].textContent = calculaTempo(tempoObjetivo2);
-contadores[2].textContent = calculaTempo(tempoObjetivo3);
-contadores[3].textContent = calculaTempo(tempoObjetivo4);
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
- 
-// Para cada objetivo na lista de contadores
-for (let i = 0; i < contadores.length; i++) {
-  //Calcular o tempo usando a função e associá-lo ao objetivo
-  contadores[i].textContent = calculaTempo(tempos[i]);
-}
-    function calculaTempo(tempoObjetivo) {}
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
 
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
 
-    return (
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  
+  return (
   dias +
   " dias " +
   horas +
   " horas " +
   minutos +
   " minutos " +
-  segundos +
+  segundos  +
   " segundos"
 );
+}
