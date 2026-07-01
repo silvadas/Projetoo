becf44fe78f1f6bddc4f1973149fd4439a4777d5
silvadas <felix.barbosa.gabriel@escola.pr.gov.br>
2026-07-01 08:38:09 -0300

    ....

--- main.js
+++ main.js
@@ -27,32 +27,39 @@ contadores[1].textContent = calculaTempo(tempoObjetivo2);
 contadores[2].textContent = calculaTempo(tempoObjetivo3);
 contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
-      function calculaTempo(tempoObjetivo) {}
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-  let segundos = Math.floor(tempoFinal / 1000);
-  let minutos = Math.floor(segundos / 60);
-  let horas = Math.floor(minutos / 60);
-  let dias = Math.floor(horas / 24);
+function calculaTempo(tempoObjetivo) { }
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
 
-  segundos %= 60;
-  minutos %= 60;
-  horas %= 24;
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
 
-  if (tempoFinal > 0) 
-    return (
-      dias +
-      " dias " +
-      horas +
-      " horas " +
-      minutos +
-      " minutos " +
-      segundos +
-      " segundos"
-   ); else {
-    {}
-    return "Prazo Finalizado";
-  }
+if (tempoFinal > 0)
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  ); else {
+  { }
+  return "Prazo Finalizado";
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
 
 
 
